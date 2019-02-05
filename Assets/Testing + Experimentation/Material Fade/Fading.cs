d1fbc9d4d6fbbdbaf50241be13fa861af70dc1c0
Jinfijizo23 <1009508@student.sae.edu.au>
2019-02-05 14:02:00 +1100

    Orb Work
    Finalised Orb spawn + movement speed. Now working on Orb Fading and minor design issue.

---
+++ Assets/Testing + Experimentation/Material Fade/Fading.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour {
+
+   Renderer R;
+
+
+    void Start()
+    {
+        R = GetComponent<Renderer>();
+        Color Alpha = R.material.color;
+        Alpha.a = 0f;
+        R.material.color = Alpha;
+        StartCoroutine("FadeIn");
+    }
+    IEnumerator FadeIn()
+    {
+        for(float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color Alpha = R.material.color;
+            Alpha.a = f;
+            R.material.color = Alpha;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
--- Assets/Testing + Experimentation/RandomSpawning/Bang.cs
+++ Assets/Testing + Experimentation/RandomSpawning/Bang.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bang : MonoBehaviour {
+    private float speed = 2.5f;
+
 
 	void Update ()
     {
-        transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
     }
 }
 
