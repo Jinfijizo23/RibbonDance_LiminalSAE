efb0f0d50f96370bb3d69d3f5ce19cac6ab2dcf9
Jinfijizo23 <1009508@student.sae.edu.au>
2019-02-06 14:04:19 +1100

    Orb feature complete
    Material fade script working and implemented into main scene along with spawning/movement script. Focusing now on design.

--- Assets/Scripts/OrbProperties.cs
+++ Assets/Scripts/OrbProperties.cs
@@ -12,21 +12,30 @@ public class OrbProperties : MonoBehaviour {
     public ParticleSystem Z;
     //public float revive;
     //public float settime = 5f;
+    private float speed = 2.5f;
+    Collider H;
+    Color Alpha;
+    float f = 0.05f;
+    public float count = 10.0f;
+
 
     void Start()
     {
         X = GetComponent<Renderer>();
+        H = GetComponent<Collider>();
+        H.enabled = false;
+        y.Pause();
+        Alpha = X.material.color;
+        Alpha.a = 0f;
+        X.material.color = Alpha;
+        StartCoroutine("FadeIn");
+
         //revive = settime;
     }
-    //void Update()
-    //{
-    //    revive -= Time.deltaTime;
-    //    //Debug.Log(revive);
-    //    if (revive <= 0)
-    //    {
-    //        Revive();
-    //    }
-    //}
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.up, speed * Time.deltaTime);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Wisp")
@@ -44,4 +53,36 @@ public class OrbProperties : MonoBehaviour {
     //    Z.Stop();
     //    revive = settime;
     //}
+    IEnumerator FadeIn()
+    {
+        while (f <= 1.0f)
+        {
+            f += 0.01f;
+            Alpha = X.material.color;
+            Alpha.a = f;
+            X.material.color = Alpha;
+            yield return new WaitForSeconds(0.05f);
+        }
+        H.enabled = true;
+        y.Play();
+        Debug.Log("Object Complete");
+        yield return new WaitForSeconds(count);
+        Debug.Log("Thanos snapped his fingers");
+        StartCoroutine(FadeOut());
+    }
+    IEnumerator FadeOut()
+    {
+        H.enabled = false;
+        y.Stop();
+        while (f >= 0.0f)
+        {
+            f -= 0.01f;
+            Alpha.a = f;
+            X.material.color = Alpha;
+            yield return new WaitForSeconds(0.05f);
+        }
+        yield return null;
+        Debug.Log("He Won");
+        Destroy(gameObject);
+    }
 }
--- Assets/Testing + Experimentation/Material Fade/Fading.cs
+++ Assets/Testing + Experimentation/Material Fade/Fading.cs
@@ -5,24 +5,51 @@ using UnityEngine;
 public class Fading : MonoBehaviour {
 
    Renderer R;
+   Collider H;
+   Color Alpha;
+    float f = 0.05f;
+    public float count = 10.0f;
 
 
     void Start()
     {
         R = GetComponent<Renderer>();
-        Color Alpha = R.material.color;
+        H = GetComponent<Collider>();
+        H.enabled = false;
+        Alpha = R.material.color;
         Alpha.a = 0f;
         R.material.color = Alpha;
         StartCoroutine("FadeIn");
     }
+
     IEnumerator FadeIn()
     {
-        for(float f = 0.05f; f <= 1; f += 0.05f)
+        while (f <= 1.0f)
         {
-            Color Alpha = R.material.color;
+            f += 0.01f;
+            Alpha = R.material.color;
             Alpha.a = f;
             R.material.color = Alpha;
             yield return new WaitForSeconds(0.05f);
         }
+        H.enabled = true;
+        Debug.Log("Object Complete");
+        yield return new WaitForSeconds(count);
+        Debug.Log("Thanos snapped his fingers");
+        StartCoroutine(FadeOut());
+    }
+    IEnumerator FadeOut()
+    {
+        H.enabled = false;
+        while (f>=0.0f)
+        {
+            f -= 0.01f;
+            Alpha.a = f;
+            R.material.color = Alpha;
+            yield return new WaitForSeconds(0.05f);
+        }     
+        yield return null/*new WaitForSeconds(3.0f)*/;
+        Debug.Log("He Won");
+        Destroy(gameObject);
     }
 }
